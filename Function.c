4d7b08d80d1186ce1172d7461d41b1795fd3f080
Ethan Coleman <ethanriley@mac.com>
2017-06-30 21:59:02 -0700

    Calculates area of circle and trapezoid, surface area of prism, and circumference of circle

--- Function.c
+++ Function.c
@@ -21,6 +21,26 @@ void a_circle(){
   printf("The area of the circle is : %lf \n", a);
 }
 
+void ci_circle(){
+  double r, ci;
+  printf("Enter the radius here: \n");
+  scanf("%lf", &r);
+  ci = (2 * M_PI) * r;
+  printf("The circumference of the circle is : %lf \n", ci);
+}
+
+void a_trapezoid(){
+  double b1, b2, h, a;
+  printf("Enter the first base here: \n");
+  scanf("%lf", &b1);
+  printf("Enter the second base here: \n");
+  scanf("%lf", &b2);
+  printf("Enter the height here: \n");
+  scanf("%lf", &h);
+  a = (b1 + b2) / 2 * h;
+  printf("The area of the trapezoid is : %lf \n", a);
+}
+
 int main(){
   int input;
 
@@ -29,6 +49,8 @@ int main(){
   while(input > 0){
     printf("Press 1 for SA of Prism \n\
 Press 2 for Area of Circle \n\
+Press 3 for Circumference of Circle \n\
+Press 4 for Area of Trapezoid \n\
 Press any negative number to stop code \n");
 
     scanf("%d", &input);
@@ -41,6 +63,12 @@ Press any negative number to stop code \n");
     }
     else if(input == 2){
       a_circle();
+    }
+    else if(input == 3){
+      ci_circle();
+    }
+    else if(input == 4){
+      a_trapezoid();
     }
     else{
       printf("Invalid number! \n");
